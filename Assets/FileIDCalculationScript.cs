738113f31b0865ab91743b6c612954d711208eb5
WouterVanmulkenUnit040 <wouter.vanmulken@unit040.com>
2019-09-16 15:48:45 +0200

    Changed namespacing

--- Assets/FileIDCalculationScript.cs
+++ Assets/FileIDCalculationScript.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using ImportExporter;
+using importerexporter;
 using UnityEngine;
 
 public class FileIDCalculationScript : MonoBehaviour
