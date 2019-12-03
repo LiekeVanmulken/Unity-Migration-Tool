22be6c5f53265c9658190096869f9e9bc146ac14
WouterVanmulkenUnit040 <wouter.vanmulken@unit040.com>
2019-12-03 17:56:51 +0100

    Forgot to include class

---
+++ Assets/MigrationTool_Prespective/Prepackage/PrepackageConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace u040.prespective.migrationtoool
+{
+    public static class PrepackageConstants
+    {
+        public const string EXTENSION = "prepackage";
+        public const string PREPACKAGE_PACKAGE_LOCATION = "MIGRATION_TOOL_UPDATER.PACKAGE_IMPORTING";
+        public const string PREPACKAGE_PACKAGE_CONTENT = "MIGRATION_TOOL_UPDATER.PACKAGE_CONTENT";
+        public const string PREPACKAGE_PACKAGE_VERSION_OLD = "MIGRATION_TOOL_UPDATER.PACKAGE_VERSION_OLD";
+        
+        public const string PREPACKAGE_DOMAIN = "http://localhost:8080";
+        public const string PREPACKAGE_VERSIONS_PATH = "/versions";
+    }
+}
